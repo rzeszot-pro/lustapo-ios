866986d3f3127a5bab005bf01d1b6311044366db
Piotr Wołoszkiewicz <czerenkow@gmail.com>
2016-08-19 12:50:58 +0200

    Initial commit

---
+++ LublinWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LublinWeather
+//
+//  Created by Piotr Woloszkiewicz on 18/08/16.
+//  Copyright © 2016 Piotr Woloszkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
