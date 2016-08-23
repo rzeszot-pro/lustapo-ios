3c04063f543713e65ecb5c8dc31874b00e97dcc4
Piotr Wołoszkiewicz <czerenkow@gmail.com>
2016-08-23 11:03:39 +0200

    Add more stations

--- LublinWeather/Model.swift
+++ LublinWeather/Model.swift
@@ -14,9 +14,21 @@ struct WeatherStation
 	let ip: String
 }
 
+private let mainURL = "http://212.182.4.252/data.php"
+private func mainURLWithParamStationID(stationId: String) -> String
+{
+	return mainURL + "?s=" + stationId
+}
+
 let weatherStationList = [
-	WeatherStation(name: "Plac Litewski i wieża", ip: "http://212.182.4.252/data.php?s=16"),
-	WeatherStation(name: "Ogród Botaniczny", ip: "http://212.182.4.252/data.php?s=10")
+	WeatherStation(name: "Plac Litewski i wieża", ip: mainURLWithParamStationID("16")),
+	WeatherStation(name: "Ogród Botaniczny", ip: mainURLWithParamStationID("10")),
+	WeatherStation(name: "Florianka", ip: mainURLWithParamStationID("12")),
+	WeatherStation(name: "Luków", ip: mainURLWithParamStationID("13")),
+	WeatherStation(name: "MPWiK Zemborzycka", ip: mainURLWithParamStationID("17")),
+	WeatherStation(name: "MPWiK Hajdów", ip: mainURLWithParamStationID("18")),
+	WeatherStation(name: "PGK Lubartow", ip: mainURLWithParamStationID("19")),
+	WeatherStation(name: "Guciów", ip: mainURLWithParamStationID("11"))
 ]
 
 struct WeatherState
