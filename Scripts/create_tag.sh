4149f7e3d739da4ebc0839372f68df38fa3025ae
Damian Rzeszot <damian.rzeszot@gmail.com>
2019-10-26 01:13:58 +0200

    Update autotag script

--- Scripts/create_tag.sh
+++ Scripts/create_tag.sh
@@ -2,7 +2,6 @@
 
 cd "${SOURCE_ROOT}"
 
-TAG_PREFIX=`echo ${TARGET_NAME} | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g'`
-TAG_NAME="release/${TAG_PREFIX}-${CURRENT_PROJECT_VERSION}+${MARKETING_VERSION}"
+TAG_NAME="release/${MARKETING_VERSION}+${CURRENT_PROJECT_VERSION}"
 
 git tag --force --message "autotag ${TAG_NAME}" "${TAG_NAME}"
