febd93cd357aea080510faaad2d180b8130da538
Damian Rzeszot <damian.rzeszot@gmail.com>
2019-10-27 10:21:38 +0100

    Add unit testing bundle

---
+++ Unit Tests/LustapoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lustapo_Tests.swift
+//  Lustapo Tests
+//
+//  Created by Damian Rzeszot on 27/10/2019.
+//  Copyright © 2019 Lubelskie Stacje Pogodowe. All rights reserved.
+//
+
+import XCTest
+
+class LustapoTests: XCTestCase {
+
+    func testExample() {
+
+    }
+
+}
